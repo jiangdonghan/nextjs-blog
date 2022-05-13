dec81f22700a9cbbd6c51841f12e8d0183460c8a
Donghan Jiang <2641635339@qq.com>
2022-05-13 14:56:39 +0800

    feat: remove uncessary code

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -5,11 +5,9 @@ import { openGraph } from '@/lib/helper';
 
 // !STARTERCONF Change these default meta
 const defaultMeta = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
-  siteName: 'Next.js + Tailwind CSS + TypeScript Starter',
-  description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
+  title: `Donghan's Blog`,
+  siteName: `Donghan's Blog`,
+  description: 'Personal Blog',
   url: 'https://tsnext-tw.thcl.dev',
   type: 'website',
   robots: 'follow, index',
