59779ff5da42c7cf136eb94a97cea9ab7599d852
Donghan Jiang <2641635339@qq.com>
2022-06-22 15:04:33 +0800

    feat: post card component

--- src/components/PostWidget.tsx
+++ src/components/PostWidget.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import moment from 'moment';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
 
-const PostWidget = () => {
-  return <div>PostWidget</div>;
+import { PostCardProps } from '@/components/PostCard';
+
+import { getRecentPosts, getSimilarPosts } from '@/services';
+
+interface PostWidgetProps {
+  categories?: string[];
+  slug?: string;
+}
+const PostWidget: React.FC<PostWidgetProps> = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState<PostCardProps[]>([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts().then((posts) => {
+        setRelatedPosts(posts);
+      });
+    } else {
+      getRecentPosts().then((posts) => {
+        setRelatedPosts(posts);
+      });
+    }
+  }, [slug, categories]);
+  return (
+    <div className='mb-8 rounded-lg bg-white p-8 shadow-lg'>
+      <h3 className='mb-8 border-b pb-4 text-xl font-semibold '>
+        {slug ? 'Related Posts' : 'Recent Posts'}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div className='flex w-full items-center' key={post.title}>
+          <div className='w-16 flex-none'>
+            <img
+              alt={post.title}
+              width='60px'
+              height='60px'
+              className='rounded-full align-middle'
+              src={post.featuredImage.url}
+            />
+          </div>
+          <div className='ml-4 flex-grow'>
+            <p className='font-xs text-gray-500'>
+              {moment(post.createdAt).format('MMM DD, YYYY')}
+            </p>
+            <Link href={`/post/${post.slug}`} key={post.title}>
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PostWidget;
--- src/services/index.ts
+++ src/services/index.ts
@@ -34,3 +34,43 @@ export const getPosts = async () => {
   const res = await request(graphqlAPI, query);
   return res.postsConnection.edges;
 };
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails {
+      posts(orderBy: createdAt_ASC, last: 3) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const res = await request(graphqlAPI, query);
+  return res.posts;
+};
+
+export const getSimilarPosts = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $category: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const res = await request(graphqlAPI, query);
+  return res.posts;
+};
